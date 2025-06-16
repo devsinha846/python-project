4da17baee83b9fbb32e7eef9288903434a2322d1
devsinha846 <161852307+devsinha846@users.noreply.github.com>
2025-06-16 11:46:09 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,16 @@
+# madlibs game
+# word game whre you can create a story 
+# by filing in blanks with random words 
+
+adjective1 = input("enter an adjective (description):")
+noun1 = input("enter a noun (person, place, thing):")
+asdjective2 = input("enter an adjective (description):")
+verb1 = input("enter a verb ending with 'ing")
+adjective3 = input("enter an adjective (description):")
+
+
+print(f"today i want to a {adjective1} zoo.")
+print(f" in an exhibt, I saw a {noun1}")
+print(f"{noun1} was {adjective1} and {verb1}")
+print(f" i was {adjective1} and verb{1}")
+print(f" I was {adjective3}!")
